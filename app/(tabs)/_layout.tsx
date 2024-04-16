8722f0f9c8ba9cf37b91f2b9545ff48e1b7fe270
Phillipe <ph.stos.udia@gmail.com>
2024-04-15 22:09:07 -0300

    branch-acess-bug-1a

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -5,172 +5,172 @@ import { StyleSheet, Pressable } from 'react-native';
 export default function TabLayout() {
   return (
     <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: '#B8D9D3',
-        tabBarInactiveTintColor: '#fff',
-        tabBarShowLabel: true,
-        tabBarStyle: {
-          position: 'absolute',
-          backgroundColor: '#282A36',
-          borderTopWidth: 0,
-          width: '90%',
-          bottom: 15,
-          alignSelf: 'center',
-          left: 15,
-          right: 15,
-          elevation: 0,
-          borderRadius: 20,
-          height: 70,
-          paddingTop: 10,
-          paddingBottom: 12,
-        },
-        tabBarLabelStyle: {
-          fontSize: 12,
-          fontWeight: 'bold',
-        },
-      }}>
-      <Tabs.Screen
-        name="home"
-        options={{
-          title: 'Home',
-          headerShown: true,
-          tabBarIcon: ({ color, size, focused }) => {
-            if (focused) {
-              return <Ionicons name="home" size={size} color={color} />;
-            }
+    screenOptions={{
+      tabBarActiveTintColor: '#B8D9D3',
+      tabBarInactiveTintColor: '#fff',
+      tabBarShowLabel: true,
+      tabBarStyle: {
+        position: 'absolute',
+        backgroundColor: '#282A36',
+        borderTopWidth: 0,
+        width: '90%',
+        bottom: 15,
+        alignSelf: 'center',
+        left: 15,
+        right: 15,
+        elevation: 0,
+        borderRadius: 20,
+        height: 70,
+        paddingTop: 10,
+        paddingBottom: 12,
+      },
+      tabBarLabelStyle: {
+        fontSize: 12,
+        fontWeight: 'bold',
+      },
+    }}>
+    <Tabs.Screen
+      name="index"
+      options={{
+        title: 'Início',
+        headerShown: true,
+        tabBarIcon: ({ color, size, focused }) => {
+          if (focused) {
+            return <Ionicons name="home" size={size} color={color} />;
+          }
 
-            return <Ionicons name="home-outline" size={size} color={color} />;
-          },
-          headerRight: () => (
-            <Link href="/profile" asChild>
-              <Pressable>
-                {({ pressed }) => (
-                  <Ionicons
-                    name="person-circle"
-                    size={48}
-                    color="#fff"
-                    style={[styles.headerRight, { opacity: pressed ? 0.5 : 1 }]}
-                  />
-                )}
-              </Pressable>
-            </Link>
-          ),
-          headerStyle: {
-            backgroundColor: '#3E4A59',
-          },
-          headerTintColor: '#fff',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-            fontSize: 25,
-          },
-        }}
-      />
-      <Tabs.Screen
-        name="celulas"
-        options={{
-          title: 'Celulas',
-          headerShown: true,
-          tabBarIcon: ({ color, size, focused }) => {
-            if (focused) {
-              return <FontAwesome6 name="people-roof" size={size} color={color} />;
-            }
+          return <Ionicons name="home-outline" size={size} color={color} />;
+        },
+        headerRight: () => (
+          <Link href="/perfil" asChild>
+            <Pressable>
+              {({ pressed }) => (
+                <Ionicons
+                  name="person-circle"
+                  size={48}
+                  color="#fff"
+                  style={[styles.headerRight, { opacity: pressed ? 0.5 : 1 }]}
+                />
+              )}
+            </Pressable>
+          </Link>
+        ),
+        headerStyle: {
+          backgroundColor: '#3E4A59',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+          fontSize: 25,
+        },
+      }}
+    />
+     <Tabs.Screen
+      name="celulas"
+      options={{
+        title: 'Celulas',
+        headerShown: true,
+        tabBarIcon: ({ color, size, focused }) => {
+          if (focused) {
             return <FontAwesome6 name="people-roof" size={size} color={color} />;
-          },
-          headerRight: () => (
-            <Link href="/profile" asChild>
-              <Pressable>
-                {({ pressed }) => (
-                  <Ionicons
-                    name="person-circle"
-                    size={48}
-                    color="#fff"
-                    style={[styles.headerRight, { opacity: pressed ? 0.5 : 1 }]}
-                  />
-                )}
-              </Pressable>
-            </Link>
-          ),
-          headerStyle: {
-            backgroundColor: '#3E4A59',
-          },
-          headerTintColor: '#fff',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-            fontSize: 25,
-          },
-        }}
-      />
-      <Tabs.Screen
-        name="eventos"
-        options={{
-          title: 'Eventos',
-          headerShown: true,
-          tabBarIcon: ({ color, focused }) => {
-            if (focused) {
-              return <MaterialIcons name="event" size={30} color={color} />;
-            }
+          }
+          return <FontAwesome6 name="people-roof" size={size} color={color} />;
+        },
+        headerRight: () => (
+          <Link href="/perfil" asChild>
+            <Pressable>
+              {({ pressed }) => (
+                <Ionicons
+                  name="person-circle"
+                  size={48}
+                  color="#fff"
+                  style={[styles.headerRight, { opacity: pressed ? 0.5 : 1 }]}
+                />
+              )}
+            </Pressable>
+          </Link>
+        ),
+        headerStyle: {
+          backgroundColor: '#3E4A59',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+          fontSize: 25,
+        },
+      }}
+    />
+    <Tabs.Screen
+      name="eventos"
+      options={{
+        title: 'Eventos',
+        headerShown: true,
+        tabBarIcon: ({ color, focused }) => {
+          if (focused) {
             return <MaterialIcons name="event" size={30} color={color} />;
-          },
-          headerRight: () => (
-            <Link href="/profile" asChild>
-              <Pressable>
-                {({ pressed }) => (
-                  <Ionicons
-                    name="person-circle"
-                    size={48}
-                    color="#fff"
-                    style={[styles.headerRight, { opacity: pressed ? 0.5 : 1 }]}
-                  />
-                )}
-              </Pressable>
-            </Link>
-          ),
-          headerStyle: {
-            backgroundColor: '#3E4A59',
-          },
-          headerTintColor: '#fff',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-            fontSize: 25,
-          },
-        }}
-      />
-      <Tabs.Screen
-        name="more"
-        options={{
-          title: 'More',
-          headerShown: true,
-          tabBarIcon: ({ color, size, focused }) => {
-            if (focused) {
-              return <AntDesign name="appstore1" size={size} color={color} />;
-            }
+          }
+          return <MaterialIcons name="event" size={30} color={color} />;
+        },
+        headerRight: () => (
+          <Link href="/perfil" asChild>
+            <Pressable>
+              {({ pressed }) => (
+                <Ionicons
+                  name="person-circle"
+                  size={48}
+                  color="#fff"
+                  style={[styles.headerRight, { opacity: pressed ? 0.5 : 1 }]}
+                />
+              )}
+            </Pressable>
+          </Link>
+        ),
+        headerStyle: {
+          backgroundColor: '#3E4A59',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+          fontSize: 25,
+        },
+      }}
+    />
+    <Tabs.Screen
+      name="more"
+      options={{
+        title: 'Mais',
+        headerShown: true,
+        tabBarIcon: ({ color, size, focused }) => {
+          if (focused) {
             return <AntDesign name="appstore1" size={size} color={color} />;
-          },
-          headerRight: () => (
-            <Link href="/profile" asChild>
-              <Pressable>
-                {({ pressed }) => (
-                  <Ionicons
-                    name="person-circle"
-                    size={48}
-                    color="#fff"
-                    style={[styles.headerRight, { opacity: pressed ? 0.5 : 1 }]}
-                  />
-                )}
-              </Pressable>
-            </Link>
-          ),
-          headerStyle: {
-            backgroundColor: '#3E4A59',
-          },
-          headerTintColor: '#fff',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-            fontSize: 25,
-          },
-        }}
-      />
-    </Tabs>
+          }
+          return <AntDesign name="appstore1" size={size} color={color} />;
+        },
+        headerRight: () => (
+          <Link href="/perfil" asChild>
+            <Pressable>
+              {({ pressed }) => (
+                <Ionicons
+                  name="person-circle"
+                  size={48}
+                  color="#fff"
+                  style={[styles.headerRight, { opacity: pressed ? 0.5 : 1 }]}
+                />
+              )}
+            </Pressable>
+          </Link>
+        ),
+        headerStyle: {
+          backgroundColor: '#3E4A59',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+          fontSize: 25,
+        },
+      }}
+    />
+  </Tabs>
   );
 }
 const styles = StyleSheet.create({
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="profile" options={{ title: 'Perfil' }} />
+      <Stack.Screen name="perfil" options={{ title: 'Perfil' }} />
     </Stack>
   );
 }
