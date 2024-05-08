32060439e680eb0afc07d6176378c7fa7aa4be73
Phillipe17Macedo <ph.stos.udia@gmail.com>
2024-05-08 18:52:48 -0300

    sucess-build-beta-v0.1.7

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -16,6 +16,7 @@ import {
   Modal,
   RefreshControl, 
 } from 'react-native';
+import { styles } from '../../style/StylesHome/styles';
 import { getStorage, ref as storageRef, uploadBytes, getDownloadURL } from 'firebase/storage';
 import { isAdmin } from '~/utils/Usuario/authAdmin';
 import { buscarEventosDoBanco } from '~/utils/Evento/buscar';
@@ -63,14 +64,12 @@ export default function Home() {
   const [refreshing, setRefreshing] = useState(false);
 
   useEffect(() => {
-    // Verifica se o usuário é administrador ao carregar a tela
     const checkAdminStatus = async () => {
       const isAdminResult = await isAdmin();
       setIsAdminUser(isAdminResult);
     };
     checkAdminStatus();
 
-    // Busca os eventos do banco de dados ao carregar a tela
     const fetchEventos = async () => {
       try {
         const eventosDoBanco = await buscarEventosDoBanco();
@@ -243,122 +242,3 @@ export default function Home() {
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#040316',
-    paddingBottom: 100,
-  },
-  areaEventos:{
-    paddingHorizontal: 25,
-  },
-  areaContainerEvento: {
-    marginTop: 10,
-  },
-  containerInputNewEvento:{
-    flex: 1,
-    position: 'relative',
-    marginTop: 10,
-    width: '85%',
-    height: 'auto',
-    alignSelf: 'center',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  inputTextoEvento: {
-    paddingVertical: 5,
-    paddingHorizontal: 15,
-    backgroundColor: '#fff',
-    borderRadius: 10,
-    borderColor: '#C0C0C0',
-    borderWidth: 1,
-    width: '100%',
-    height: 40,
-    marginBottom: 5,
-  },
-  containerIconeAddEvento: {
-    width: 40,
-    height: 40,
-    backgroundColor: '#fff',
-    borderRadius: 60,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderColor: '#C0C0C0',
-    borderWidth: 1,
-    marginBottom: 5,
-  },
-  iconeAddEvento: {
-    fontSize: 30,
-    fontWeight: 'normal',
-    paddingBottom: 3,
-    color: 'gray',
-  },
-  removerEventoButton: {
-    position: 'absolute',
-    top: 22,
-    right: 8,
-    backgroundColor: '#3E4A59',
-    paddingVertical: 5,
-    paddingHorizontal: 8,
-    borderRadius: 7,
-    borderWidth: 1,
-    borderColor: '#B8D9D3',
-    shadowOffset:{width:5,height:5},
-    shadowOpacity: 0.5,
-    shadowRadius: 10,
-    elevation: 5,  
-  },
-  removerEventoButtonText: {
-    color: 'white',
-    fontWeight: 'bold',
-  },
-  centeredView: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: 22,
-  },
-  modalView: {
-    margin: 20,
-    backgroundColor: 'white',
-    borderRadius: 20,
-    padding: 35,
-    alignItems: 'center',
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5,
-  },
-  modalText: {
-    marginBottom: 15,
-    textAlign: 'center',
-  },
-  buttonsContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    width: '100%',
-  },
-  button: {
-    borderRadius: 20,
-    padding: 10,
-    elevation: 2,
-    minWidth: '40%',
-    alignItems: 'center',
-  },
-  confirmButton: {
-    backgroundColor: '#2196F3',
-  },
-  cancelButton: {
-    backgroundColor: '#FF5733',
-  },
-  textStyle: {
-    color: 'white',
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-});
