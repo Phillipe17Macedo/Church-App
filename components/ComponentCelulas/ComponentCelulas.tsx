cbd28173b3e8a049a2bf10c92337717e675d9bee
Phillipe17Macedo <ph.stos.udia@gmail.com>
2024-05-10 15:33:15 -0300

    sucess-build-beta-v0.2.4

---
+++ components/ComponentCelulas/ComponentCelulas.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import { Text, View, TouchableOpacity, Image } from 'react-native';
+
+import { styles } from '../ComponentCelulas/styles';
--- components/ComponentEventos/ComponentEventos.tsx
+++ components/ComponentEventos/ComponentEventos.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
+import { Text, View, TouchableOpacity, Image } from 'react-native';
 
 import { styles } from '../../components/ComponentEventos/styles'
 
