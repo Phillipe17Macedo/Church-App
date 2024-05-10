c1aa57b48a5f6c6254aaff59e6911135ae5e6ed9
Phillipe17Macedo <ph.stos.udia@gmail.com>
2024-05-10 20:33:14 -0300

    sucess-build-beta-v0.2.4c

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,7 +3,6 @@ import { launchImageLibraryAsync } from 'expo-image-picker';
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
 import {
-  StyleSheet,
   ScrollView,
   SafeAreaView,
   KeyboardAvoidingView,
