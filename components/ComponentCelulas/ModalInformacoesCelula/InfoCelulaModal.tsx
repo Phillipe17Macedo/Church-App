b4b39d3adda8ea731d0c97b745b5c449e6f3adb6
Phillipe17Macedo <ph.stos.udia@gmail.com>
2024-05-30 17:49:58 -0300

    backEnd-Celula-Modal-Info-0.0.3

--- components/ComponentCelulas/ModalInformacoesCelula/InfoCelulaModal.tsx
+++ components/ComponentCelulas/ModalInformacoesCelula/InfoCelulaModal.tsx
@@ -22,27 +22,27 @@ const InfoCelulaModal: React.FC<InfoCelulaModalProps> = ({ visible, celula, onCl
       visible={visible}
       onRequestClose={onClose}
     >
-      <View style={styles.centeredView}>
+      <View style={styles.ViewCentralizada}>
         <View style={styles.modalView}>
           {celula ? (
             <>
-              <Text style={styles.modalText}>Nome da Célula: {celula.titulo}</Text>
-              <Text style={styles.modalText}>Data: {celula.data}</Text>
-              <Text style={styles.modalText}>Horário: {celula.horario}</Text>
-              <Text style={styles.modalText}>Endereço: {celula.endereco}</Text>
-              <Text style={styles.modalText}>Nome do Líder: {celula.nomeLider}</Text>
-              <Text style={styles.modalText}>Número do Líder: {celula.numeroLider}</Text>
-              <Text style={styles.modalText} onPress={handleOpenMaps}>Link do Endereço Maps: {celula.linkEnderecoMaps}</Text>
-              <Text style={styles.modalText}>Descrição: {celula.descricao}</Text>
+              <Text style={styles.textoPadraoModal}>Nome da Célula: <Text style={styles.textoInformacaoCelula}>{celula.titulo}</Text></Text>
+              <Text style={styles.textoPadraoModal}>Data: <Text style={styles.textoInformacaoCelula}>{celula.data}</Text></Text>
+              <Text style={styles.textoPadraoModal}>Horário: <Text style={styles.textoInformacaoCelula}>{celula.horario}</Text></Text>
+              <Text style={styles.textoPadraoModal}>Endereço: <Text style={styles.textoInformacaoCelula}>{celula.endereco}</Text></Text>
+              <Text style={styles.textoPadraoModal}>Nome do Líder: <Text style={styles.textoInformacaoCelula}>{celula.nomeLider}</Text></Text>
+              <Text style={styles.textoPadraoModal}>Número do Líder: <Text style={styles.textoInformacaoCelula}>{celula.numeroLider}</Text></Text>
+              <Text style={styles.textoPadraoModal} onPress={handleOpenMaps}><Text style={styles.textoLinkMaps}>Link do Endereço no Google Maps</Text></Text>
+              <Text style={styles.textoPadraoModal}>Descrição: <Text style={styles.textoInformacaoCelula}>{celula.descricao}</Text></Text>
             </>
           ) : (
-            <Text style={styles.modalText}>Carregando...</Text>
+            <Text style={styles.textoPadraoModal}>Carregando...</Text>
           )}
           <TouchableOpacity
-            style={[styles.button, styles.buttonClose]}
+            style={[styles.botaoSair]}
             onPress={onClose}
           >
-            <Text style={styles.textStyle}>Fechar</Text>
+            <Text style={styles.textoBotao}>Fechar</Text>
           </TouchableOpacity>
         </View>
       </View>
