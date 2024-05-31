29e28450e6fc22b0a0969d8210c4ed34e125fd2a
Phillipe17Macedo <ph.stos.udia@gmail.com>
2024-05-31 14:33:57 -0300

    backEnd-Home-Modal-Info-0.0.2

--- connection/Evento/remover.ts
+++ connection/Evento/remover.ts
@@ -1,37 +1,38 @@
-import { initializeApp } from 'firebase/app';
-import { getDatabase, ref, push, set, get, update, DataSnapshot, remove } from 'firebase/database';
-
+import { initializeApp } from "firebase/app";
+import {
+  getDatabase,
+  ref,
+  push,
+  set,
+  get,
+  update,
+  DataSnapshot,
+  remove,
+} from "firebase/database";
 
 const firebaseConfig = {
-    apiKey: 'AIzaSyCyhMAnKhc2y_2EzP2LyO7-AbVEBjgj2ek',
-    authDomain: 'videiraapp.firebaseapp.com',
-    databaseURL: 'https://videiraapp-default-rtdb.firebaseio.com',
-    projectId: 'videiraapp',
-    storageBucket: 'videiraapp.appspot.com',
-    messagingSenderId: '348499807286',
-    appId: '1:348499807286:web:c9bcbc26c1c9acad9adc97',
-    measurementId: 'G-KHDPWMVYNN',
+  apiKey: "AIzaSyCyhMAnKhc2y_2EzP2LyO7-AbVEBjgj2ek",
+  authDomain: "videiraapp.firebaseapp.com",
+  databaseURL: "https://videiraapp-default-rtdb.firebaseio.com",
+  projectId: "videiraapp",
+  storageBucket: "videiraapp.appspot.com",
+  messagingSenderId: "348499807286",
+  appId: "1:348499807286:web:c9bcbc26c1c9acad9adc97",
+  measurementId: "G-KHDPWMVYNN",
 };
-  
-interface Evento {
-    id: string;
-    titulo: string;
-    data: string;
-    horario: string;
-    imagem: string;
-}
-  
-  const firebaseApp = initializeApp(firebaseConfig);
-  const database = getDatabase(firebaseApp);
-  
-  export const removerEventoDoBanco = async (eventoId: string) => {
-    try {
-      const eventoRef = ref(database, `eventos/${eventoId}`);
-      await remove(eventoRef);
-      console.log('Evento removido com sucesso!');
-    } catch (error) {
-      console.error('Erro ao remover evento:', error);
-      throw error;
-    }
-  };
 
+import { Evento } from "@/types";
+
+const firebaseApp = initializeApp(firebaseConfig);
+const database = getDatabase(firebaseApp);
+
+export const removerEventoDoBanco = async (eventoId: string) => {
+  try {
+    const eventoRef = ref(database, `eventos/${eventoId}`);
+    await remove(eventoRef);
+    console.log("Evento removido com sucesso!");
+  } catch (error) {
+    console.error("Erro ao remover evento:", error);
+    throw error;
+  }
+};
