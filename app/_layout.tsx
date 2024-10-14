af5501fc13939aa75a09fcd3316d9867c4dc5f43
Phillipe17Macedo <ph.stos.udia@gmail.com>
2024-10-14 18:38:17 -0300

    formatando-layout-app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,56 +1,54 @@
-/* eslint-disable prettier/prettier */
-import { Stack } from 'expo-router';
+import { Stack } from "expo-router";
 export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
+  initialRouteName: "(tabs)",
 };
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen 
-        name="(tabs)" 
-        options={{ 
+      <Stack.Screen
+        name="(tabs)"
+        options={{
           headerShown: false,
-        }} 
+        }}
       />
       <Stack.Screen
         name="perfil"
         options={{
-          title: 'Perfil',
+          title: "Perfil",
           headerStyle: {
-            backgroundColor: '#3E4A59',
+            backgroundColor: "#3E4A59",
           },
-          headerTintColor: '#fff',
+          headerTintColor: "#fff",
           headerTitleStyle: {
-            fontWeight: 'bold',
+            fontWeight: "bold",
             fontSize: 25,
           },
         }}
       />
-      <Stack.Screen 
-        name='login' 
+      <Stack.Screen
+        name="login"
         options={{
-          title: 'Login',
+          title: "Login",
           headerStyle: {
-            backgroundColor: '#3E4A59',
+            backgroundColor: "#3E4A59",
           },
-          headerTintColor: '#fff',
+          headerTintColor: "#fff",
           headerTitleStyle: {
-            fontWeight: 'bold',
+            fontWeight: "bold",
             fontSize: 25,
           },
-        }} 
+        }}
       />
-      <Stack.Screen 
-        name='sign' 
+      <Stack.Screen
+        name="sign"
         options={{
-          title: 'Cadastro',
+          title: "Cadastro",
           headerStyle: {
-            backgroundColor: '#3E4A59',
+            backgroundColor: "#3E4A59",
           },
-          headerTintColor: '#fff',
+          headerTintColor: "#fff",
           headerTitleStyle: {
-            fontWeight: 'bold',
+            fontWeight: "bold",
             fontSize: 25,
           },
         }}
