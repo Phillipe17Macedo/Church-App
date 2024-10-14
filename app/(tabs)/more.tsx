9aae0824bab0e5abd7c2291b3ce30fab60f28dd4
Phillipe17Macedo <ph.stos.udia@gmail.com>
2024-10-14 18:41:16 -0300

    formatando-tela-more

--- app/(tabs)/more.tsx
+++ app/(tabs)/more.tsx
@@ -1,25 +1,41 @@
-/* eslint-disable prettier/prettier */
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import { View, Text, ScrollView } from 'react-native';
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { View, Text, ScrollView } from "react-native";
 
-import { styles } from '../../style/StylesMore/styles'
-import OptionsMore from '@/components/OptionsScreenMore/OptionsMore';
+import { styles } from "../../style/StylesMore/styles";
+import OptionsMore from "@/components/OptionsScreenMore/OptionsMore";
 
 export default function More() {
   return (
-    <ScrollView style={styles.container} >
+    <ScrollView style={styles.container}>
       <StatusBar style="light" />
-      <Stack.Screen options={{ title: 'Mais' }} 
-      />
-        <View style={[{backgroundColor: '#3E4A59',}, {alignItems: 'center'}, {marginTop: 25},{marginBottom: 20}, {width: '100%'}, {justifyContent: 'center'}, {alignSelf: 'center'}, {borderRadius: 10}]}>
-          <Text style={[{fontSize: 21}, {paddingHorizontal: 2}, {paddingVertical: 3}, {color: '#fff'}, {textAlign: 'justify'}]}>
-            ESTA TELA SERÁ LANÇADA EM BREVE COM SUAS FUNCIONALIDADES
-          </Text>
-        </View>
+      <Stack.Screen options={{ title: "Mais" }} />
+      <View
+        style={[
+          { backgroundColor: "#3E4A59" },
+          { alignItems: "center" },
+          { marginTop: 25 },
+          { marginBottom: 20 },
+          { width: "100%" },
+          { justifyContent: "center" },
+          { alignSelf: "center" },
+          { borderRadius: 10 },
+        ]}
+      >
+        <Text
+          style={[
+            { fontSize: 21 },
+            { paddingHorizontal: 2 },
+            { paddingVertical: 3 },
+            { color: "#fff" },
+            { textAlign: "justify" },
+          ]}
+        >
+          ESTA TELA SERÁ LANÇADA EM BREVE COM SUAS FUNCIONALIDADES
+        </Text>
+      </View>
 
-        <OptionsMore/>
-
+      <OptionsMore />
     </ScrollView>
   );
 }
