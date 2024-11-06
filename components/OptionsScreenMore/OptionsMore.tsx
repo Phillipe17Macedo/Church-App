d539bbed9053e957070bdb3a89542e5b8a31ff45
Phillipe Macedo <63377161+Phillipe17Macedo@users.noreply.github.com>
2024-11-06 16:27:25 -0300

    ajustando-componente-optionsMore

--- components/OptionsScreenMore/OptionsMore.tsx
+++ components/OptionsScreenMore/OptionsMore.tsx
@@ -1,20 +1,75 @@
-import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
-
-import { styles } from './styles';
+import React from "react";
+import { View, Text, TouchableOpacity, Alert, Linking } from "react-native";
+import { styles } from "./styles";
 
 export default function OptionsMore() {
+  const pixLink =
+    "pix://00020126330014BR.GOV.BCB.PIX0114+5511999999990520Pagamento1234500203BR5925Nome+da+Organizacao6009SAO+PAULO62070503***";
+
+  const handleBibleRedirect = () => {
+    Alert.alert(
+      "Redirecionar para a Bíblia Online",
+      "Deseja ir para a Bíblia Online?",
+      [
+        { text: "Cancelar", style: "cancel" },
+        {
+          text: "Ir",
+          onPress: () => Linking.openURL("https://www.bibliaon.com/"),
+        },
+      ]
+    );
+  };
+
+  const handleWordsRedirect = () => {
+    Alert.alert(
+      "Redirecionar para o YouTube",
+      "Deseja ir para o canal do YouTube?",
+      [
+        { text: "Cancelar", style: "cancel" },
+        {
+          text: "Ir",
+          onPress: () =>
+            Linking.openURL("https://www.youtube.com/@praluizioasilva"),
+        },
+      ]
+    );
+  };
+
+  const handlePixRedirect = () => {
+    Alert.alert(
+      "Pagamento via Pix",
+      "Deseja abrir o app de pagamento para fazer um Pix?",
+      [
+        { text: "Cancelar", style: "cancel" },
+        {
+          text: "Ir",
+          onPress: async () => {
+            const supported = await Linking.canOpenURL(pixLink);
+            if (supported) {
+              Linking.openURL(pixLink);
+            } else {
+              Alert.alert(
+                "Erro",
+                "Nenhum app de pagamento compatível foi encontrado."
+              );
+            }
+          },
+        },
+      ]
+    );
+  };
+
   return (
     <View style={styles.container}>
-        <TouchableOpacity>
-            <Text style={[{color: 'black', padding: 5, backgroundColor: 'lightgray', marginBottom: 10,}]}>Dizímos & Ofertas</Text>
-        </TouchableOpacity>
-        <TouchableOpacity>
-            <Text style={[{color: 'black', padding: 5, backgroundColor: 'lightgray', marginBottom: 10,}]}>Palavras</Text>
-        </TouchableOpacity>
-        <TouchableOpacity>
-            <Text style={[{color: 'black', padding: 5, backgroundColor: 'lightgray', marginBottom: 10,}]}>Bíblia</Text>
-        </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={handlePixRedirect}>
+        <Text style={styles.buttonText}>Dizímos & Ofertas</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={handleWordsRedirect}>
+        <Text style={styles.buttonText}>Palavras</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={handleBibleRedirect}>
+        <Text style={styles.buttonText}>Bíblia</Text>
+      </TouchableOpacity>
     </View>
   );
 }
--- components/OptionsScreenMore/styles.tsx
+++ components/OptionsScreenMore/styles.tsx
@@ -1,13 +1,34 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
   container: {
-    width: '100%',
-    alignSelf: 'center',
-    backgroundColor: 'blue',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 10,
+    width: "100%",
+    alignSelf: "center",
+    backgroundColor: "#3E4A59",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 20,
+    paddingHorizontal: 15,
     borderRadius: 10,
-  }
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 3,
+    marginTop: 25,
+  },
+  button: {
+    backgroundColor: "#FFFFFF",
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    borderRadius: 8,
+    width: "100%",
+    alignItems: "center",
+    marginBottom: 15,
+  },
+  buttonText: {
+    color: "#1C2026",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
 });
