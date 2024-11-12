1f3d45728bab51bb79a4fb6f82eafd986a5465df
Phillipe Macedo <63377161+Phillipe17Macedo@users.noreply.github.com>
2024-11-12 16:26:17 -0300

    ajustando-modal-info-eventos

--- components/ComponentEventos/ModalInformacoesEvento/InfoEventoModal.tsx
+++ components/ComponentEventos/ModalInformacoesEvento/InfoEventoModal.tsx
@@ -1,15 +1,11 @@
-import React from "react";
-import {
-  Modal,
-  View,
-  Text,
-  TouchableOpacity,
-  Linking,
-  Alert,
-} from "react-native";
+// InfoEventoModal.tsx
+
+import React, { useState } from "react";
+import { Modal, View, Text, TouchableOpacity, Linking } from "react-native";
 import { Evento } from "@/types";
 import { styles } from "./styles";
 import { Fontisto } from "@expo/vector-icons";
+import ContactOptionsModal from "./../ContactOptionsModal/index";
 
 interface InfoEventoModalProps {
   visible: boolean;
@@ -22,109 +18,113 @@ const InfoEventoModal: React.FC<InfoEventoModalProps> = ({
   evento,
   onClose,
 }) => {
+  const [contactModalVisible, setContactModalVisible] = useState(false);
+
   const handleOpenMaps = () => {
     if (evento && evento.linkEnderecoMaps) {
       Linking.openURL(evento.linkEnderecoMaps);
     }
   };
 
-  const handleContactOptions = () => {
-    if (evento && evento.numeroContato) {
-      const formattedNumber = evento.numeroContato.replace(/\D/g, ""); // Remove caracteres não numéricos
+  const openContactOptions = () => {
+    setContactModalVisible(true);
+  };
 
-      Alert.alert(
-        "Contato do Evento",
-        "Escolha o aplicativo para entrar em contato:",
-        [
-          {
-            text: "Telefone",
-            onPress: () => Linking.openURL(`tel:${formattedNumber}`),
-          },
-          {
-            text: "WhatsApp",
-            onPress: () => Linking.openURL(`https://wa.me/${formattedNumber}`),
-          },
-          {
-            text: "Telegram",
-            onPress: () => Linking.openURL(`https://t.me/+${formattedNumber}`),
-          },
-          {
-            text: "Cancelar",
-            style: "cancel",
-          },
-        ]
-      );
+  const handleContactOption = (option: string) => {
+    if (evento && evento.numeroContato) {
+      const formattedNumber = evento.numeroContato.replace(/\D/g, "");
+      if (option === "Telefone") {
+        Linking.openURL(`tel:${formattedNumber}`);
+      } else if (option === "WhatsApp") {
+        Linking.openURL(`https://wa.me/${formattedNumber}`);
+      } else if (option === "Telegram") {
+        Linking.openURL(`https://t.me/+${formattedNumber}`);
+      }
     }
+    setContactModalVisible(false); // Fecha o modal de contato
   };
 
   return (
-    <Modal
-      animationType="slide"
-      transparent={true}
-      visible={visible}
-      onRequestClose={onClose}
-    >
-      <View style={styles.ViewCentralizada}>
-        <View style={styles.modalView}>
-          {evento ? (
-            <>
-              <Text style={styles.textoPadraoModal}>
-                Nome do Evento:{" "}
-                <Text style={styles.textoInformacaoEvento}>
-                  {evento.titulo}
+    <>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={visible}
+        onRequestClose={onClose}
+      >
+        <View style={styles.ViewCentralizada}>
+          <View style={styles.modalView}>
+            {evento ? (
+              <>
+                <Text style={styles.textoPadraoModal}>
+                  Nome do Evento:{" "}
+                  <Text style={styles.textoInformacaoEvento}>
+                    {evento.titulo}
+                  </Text>
                 </Text>
-              </Text>
-              <Text style={styles.textoPadraoModal}>
-                Data do Evento:{" "}
-                <Text style={styles.textoInformacaoEvento}>{evento.data}</Text>
-              </Text>
-              <Text style={styles.textoPadraoModal}>
-                Horário do Evento:{" "}
-                <Text style={styles.textoInformacaoEvento}>
-                  {evento.horario}
+                <Text style={styles.textoPadraoModal}>
+                  Data do Evento:{" "}
+                  <Text style={styles.textoInformacaoEvento}>
+                    {evento.data}
+                  </Text>
                 </Text>
-              </Text>
-              <Text style={styles.textoPadraoModal}>
-                Endereço:{" "}
-                <Text style={styles.textoInformacaoEvento}>
-                  {evento.endereco}
+                <Text style={styles.textoPadraoModal}>
+                  Horário do Evento:{" "}
+                  <Text style={styles.textoInformacaoEvento}>
+                    {evento.horario}
+                  </Text>
                 </Text>
-              </Text>
-              <Text style={styles.textoPadraoModal} onPress={handleOpenMaps}>
-                <Text style={styles.textoLinkMaps}>
-                  <Fontisto name="map" size={18} color="#2196F3" />
-                  Link do Endereço no Google Maps
+                <Text style={styles.textoPadraoModal}>
+                  Endereço:{" "}
+                  <Text style={styles.textoInformacaoEvento}>
+                    {evento.endereco}
+                  </Text>
                 </Text>
-              </Text>
-              <Text style={styles.textoPadraoModal}>
-                Número de Contato:{" "}
-                <Text
-                  style={styles.textoNumeroContato}
-                  onPress={handleContactOptions}
-                >
-                  {evento.numeroContato}
+                <Text style={styles.textoPadraoModal} onPress={handleOpenMaps}>
+                  <Text style={styles.textoLinkMaps}>
+                    <Fontisto name="map" size={18} color="#2196F3" />
+                    Link do Endereço no Google Maps
+                  </Text>
                 </Text>
-              </Text>
-              <Text style={styles.textoPadraoModal}>
-                Valor:{" "}
-                <Text style={styles.textoInformacaoEvento}>{evento.valor}</Text>
-              </Text>
-              <Text style={styles.textoPadraoModal}>
-                Descrição:{" "}
-                <Text style={styles.textoInformacaoEvento}>
-                  {evento.descricao}
+                <Text style={styles.textoPadraoModal}>
+                  Número de Contato:{" "}
+                  <Text
+                    style={styles.textoNumeroContato}
+                    onPress={openContactOptions}
+                  >
+                    {evento.numeroContato}
+                  </Text>
                 </Text>
-              </Text>
-            </>
-          ) : (
-            <Text style={styles.textoPadraoModal}>Carregando...</Text>
-          )}
-          <TouchableOpacity style={[styles.botaoSair]} onPress={onClose}>
-            <Text style={styles.textoBotao}>Fechar</Text>
-          </TouchableOpacity>
+                <Text style={styles.textoPadraoModal}>
+                  Valor:{" "}
+                  <Text style={styles.textoInformacaoEvento}>
+                    {evento.valor}
+                  </Text>
+                </Text>
+                <Text style={styles.textoPadraoModal}>
+                  Descrição:{" "}
+                  <Text style={styles.textoInformacaoEvento}>
+                    {evento.descricao}
+                  </Text>
+                </Text>
+              </>
+            ) : (
+              <Text style={styles.textoPadraoModal}>Carregando...</Text>
+            )}
+            <TouchableOpacity style={styles.botaoSair} onPress={onClose}>
+              <Text style={styles.textoBotao}>Fechar</Text>
+            </TouchableOpacity>
+          </View>
         </View>
-      </View>
-    </Modal>
+      </Modal>
+
+      {/* Uso do novo ContactOptionsModal */}
+      <ContactOptionsModal
+        visible={contactModalVisible}
+        onSelectOption={handleContactOption}
+        onClose={() => setContactModalVisible(false)}
+      />
+    </>
   );
 };
 
