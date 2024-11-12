9f332dc6e65d8c4b0a94485cb3418280e70ae6ab
Phillipe Macedo <63377161+Phillipe17Macedo@users.noreply.github.com>
2024-11-12 16:26:49 -0300

    ajustando-tela-opcoes-more

--- components/OptionsScreenMore/OptionsMore.tsx
+++ components/OptionsScreenMore/OptionsMore.tsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, TouchableOpacity, Alert, Linking } from "react-native";
+import ModalPixPayment from "./ModalPixPayment"; // Certifique-se de ajustar o caminho
 import { styles } from "./styles";
 
 export default function OptionsMore() {
-  const pixLink =
-    "pix://00020126330014BR.GOV.BCB.PIX0114+5511999999990520Pagamento1234500203BR5925Nome+da+Organizacao6009SAO+PAULO62070503***";
+  const [pixModalVisible, setPixModalVisible] = useState(false);
 
   const handleBibleRedirect = () => {
     Alert.alert(
@@ -35,33 +35,12 @@ export default function OptionsMore() {
     );
   };
 
-  const handlePixRedirect = () => {
-    Alert.alert(
-      "Pagamento via Pix",
-      "Deseja abrir o app de pagamento para fazer um Pix?",
-      [
-        { text: "Cancelar", style: "cancel" },
-        {
-          text: "Ir",
-          onPress: async () => {
-            const supported = await Linking.canOpenURL(pixLink);
-            if (supported) {
-              Linking.openURL(pixLink);
-            } else {
-              Alert.alert(
-                "Erro",
-                "Nenhum app de pagamento compatível foi encontrado."
-              );
-            }
-          },
-        },
-      ]
-    );
-  };
+  const openPixModal = () => setPixModalVisible(true);
+  const closePixModal = () => setPixModalVisible(false);
 
   return (
     <View style={styles.container}>
-      <TouchableOpacity style={styles.button} onPress={handlePixRedirect}>
+      <TouchableOpacity style={styles.button} onPress={openPixModal}>
         <Text style={styles.buttonText}>Dizímos & Ofertas</Text>
       </TouchableOpacity>
       <TouchableOpacity style={styles.button} onPress={handleWordsRedirect}>
@@ -70,6 +49,9 @@ export default function OptionsMore() {
       <TouchableOpacity style={styles.button} onPress={handleBibleRedirect}>
         <Text style={styles.buttonText}>Bíblia</Text>
       </TouchableOpacity>
+
+      {/* Modal de Pagamento Pix */}
+      <ModalPixPayment visible={pixModalVisible} onClose={closePixModal} />
     </View>
   );
 }
