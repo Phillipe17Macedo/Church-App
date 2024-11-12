92c002203667a9f0910e2a3bfe7a36ef93126e99
Phillipe Macedo <63377161+Phillipe17Macedo@users.noreply.github.com>
2024-11-12 16:25:28 -0300

    ajustando-modal-info-celulas

--- components/ComponentCelulas/ModalInformacoesCelula/InfoCelulaModal.tsx
+++ components/ComponentCelulas/ModalInformacoesCelula/InfoCelulaModal.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Modal,
   View,
@@ -10,6 +10,7 @@ import {
 import { Celula } from "@/types";
 import { styles } from "./styles";
 import { Fontisto } from "@expo/vector-icons";
+import ContactOptionsModal from "./../../ComponentEventos/ContactOptionsModal/index";
 
 interface InfoCelulaModalProps {
   visible: boolean;
@@ -22,111 +23,111 @@ const InfoCelulaModal: React.FC<InfoCelulaModalProps> = ({
   celula,
   onClose,
 }) => {
+  const [contactModalVisible, setContactModalVisible] = useState(false);
   const handleOpenMaps = () => {
     if (celula && celula.linkEnderecoMaps) {
       Linking.openURL(celula.linkEnderecoMaps);
     }
   };
 
-  const handleContactOptions = () => {
-    if (celula && celula.numeroLider) {
-      const formattedNumber = celula.numeroLider.replace(/\D/g, ""); // Remove non-digit characters
+  const openContactOptions = () => {
+    setContactModalVisible(true);
+  };
 
-      Alert.alert(
-        "Contato do Líder",
-        "Escolha o aplicativo para entrar em contato:",
-        [
-          {
-            text: "Telefone",
-            onPress: () => Linking.openURL(`tel:${formattedNumber}`),
-          },
-          {
-            text: "WhatsApp",
-            onPress: () => Linking.openURL(`https://wa.me/${formattedNumber}`),
-          },
-          {
-            text: "Telegram",
-            onPress: () => Linking.openURL(`https://t.me/+${formattedNumber}`),
-          },
-          {
-            text: "Cancelar",
-            style: "cancel",
-          },
-        ]
-      );
+  const handleContactOption = (option: string) => {
+    if (celula && celula.numeroLider) {
+      const formattedNumber = celula.numeroLider.replace(/\D/g, "");
+      if (option === "Telefone") {
+        Linking.openURL(`tel:${formattedNumber}`);
+      } else if (option === "WhatsApp") {
+        Linking.openURL(`https://wa.me/${formattedNumber}`);
+      } else if (option === "Telegram") {
+        Linking.openURL(`https://t.me/+${formattedNumber}`);
+      }
     }
+    setContactModalVisible(false); // Fecha o modal de contato
   };
 
   return (
-    <Modal
-      animationType="slide"
-      transparent={true}
-      visible={visible}
-      onRequestClose={onClose}
-    >
-      <View style={styles.ViewCentralizada}>
-        <View style={styles.modalView}>
-          {celula ? (
-            <>
-              <Text style={styles.textoPadraoModal}>
-                Nome da Célula:{" "}
-                <Text style={styles.textoInformacaoCelula}>
-                  {celula.titulo}
+    <>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={visible}
+        onRequestClose={onClose}
+      >
+        <View style={styles.ViewCentralizada}>
+          <View style={styles.modalView}>
+            {celula ? (
+              <>
+                <Text style={styles.textoPadraoModal}>
+                  Nome da Célula:{" "}
+                  <Text style={styles.textoInformacaoCelula}>
+                    {celula.titulo}
+                  </Text>
                 </Text>
-              </Text>
-              <Text style={styles.textoPadraoModal}>
-                Data:{" "}
-                <Text style={styles.textoInformacaoCelula}>{celula.data}</Text>
-              </Text>
-              <Text style={styles.textoPadraoModal}>
-                Horário:{" "}
-                <Text style={styles.textoInformacaoCelula}>
-                  {celula.horario}
+                <Text style={styles.textoPadraoModal}>
+                  Data:{" "}
+                  <Text style={styles.textoInformacaoCelula}>
+                    {celula.data}
+                  </Text>
                 </Text>
-              </Text>
-              <Text style={styles.textoPadraoModal}>
-                Endereço:{" "}
-                <Text style={styles.textoInformacaoCelula}>
-                  {celula.endereco}
+                <Text style={styles.textoPadraoModal}>
+                  Horário:{" "}
+                  <Text style={styles.textoInformacaoCelula}>
+                    {celula.horario}
+                  </Text>
                 </Text>
-              </Text>
-              <Text style={styles.textoPadraoModal}>
-                Nome do Líder:{" "}
-                <Text style={styles.textoInformacaoCelula}>
-                  {celula.nomeLider}
+                <Text style={styles.textoPadraoModal}>
+                  Endereço:{" "}
+                  <Text style={styles.textoInformacaoCelula}>
+                    {celula.endereco}
+                  </Text>
                 </Text>
-              </Text>
-              <Text style={styles.textoPadraoModal}>
-                Número do Líder:{" "}
-                <Text
-                  style={styles.textoNumeroLider}
-                  onPress={handleContactOptions}
-                >
-                  {celula.numeroLider}
+                <Text style={styles.textoPadraoModal}>
+                  Nome do Líder:{" "}
+                  <Text style={styles.textoInformacaoCelula}>
+                    {celula.nomeLider}
+                  </Text>
                 </Text>
-              </Text>
-              <Text style={styles.textoPadraoModal} onPress={handleOpenMaps}>
-                <Text style={styles.textoLinkMaps}>
-                  <Fontisto name="map" size={18} color="#2196F3" />
-                  Link do Endereço no Google Maps
+                <Text style={styles.textoPadraoModal}>
+                  Número do Líder:{" "}
+                  <Text
+                    style={styles.textoNumeroLider}
+                    onPress={openContactOptions}
+                  >
+                    {celula.numeroLider}
+                  </Text>
                 </Text>
-              </Text>
-              <Text style={styles.textoPadraoModal}>
-                Descrição:{" "}
-                <Text style={styles.textoInformacaoCelula}>
-                  {celula.descricao}
+                <Text style={styles.textoPadraoModal} onPress={handleOpenMaps}>
+                  <Text style={styles.textoLinkMaps}>
+                    <Fontisto name="map" size={18} color="#2196F3" />
+                    Link do Endereço no Google Maps
+                  </Text>
                 </Text>
-              </Text>
-            </>
-          ) : (
-            <Text style={styles.textoPadraoModal}>Carregando...</Text>
-          )}
-          <TouchableOpacity style={[styles.botaoSair]} onPress={onClose}>
-            <Text style={styles.textoBotao}>Fechar</Text>
-          </TouchableOpacity>
+                <Text style={styles.textoPadraoModal}>
+                  Descrição:{" "}
+                  <Text style={styles.textoInformacaoCelula}>
+                    {celula.descricao}
+                  </Text>
+                </Text>
+              </>
+            ) : (
+              <Text style={styles.textoPadraoModal}>Carregando...</Text>
+            )}
+            <TouchableOpacity style={[styles.botaoSair]} onPress={onClose}>
+              <Text style={styles.textoBotao}>Fechar</Text>
+            </TouchableOpacity>
+          </View>
         </View>
-      </View>
-    </Modal>
+      </Modal>
+
+      <ContactOptionsModal
+        visible={contactModalVisible}
+        onSelectOption={handleContactOption}
+        onClose={() => setContactModalVisible(false)}
+      />
+    </>
   );
 };
 
