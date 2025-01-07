fb2f2d5a71fff95faa180ef1ad9f3e6bde894a59
Phillipe Macedo <63377161+Phillipe17Macedo@users.noreply.github.com>
2025-01-06 21:24:27 -0300

    backup-tela-eventos

---
+++ backup/eventos.tsx
@@ -0,0 +1,26 @@
+/* eslint-disable prettier/prettier */
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Text, SafeAreaView, View } from 'react-native';
+
+import { styles } from '../style/StylesEventos/styles';
+import { ComponentAgenda } from '@/components/Agenda/ComponentAgenda';
+
+export default function Eventos() {
+  return (
+    <SafeAreaView style={[styles.container]}>
+      <StatusBar style="light" />
+      <Stack.Screen options={{ title: 'Eventos' }} />
+
+      <View style={[{backgroundColor: '#3E4A59',}, {alignItems: 'center'}, {marginTop: 25},{marginBottom: 20}, {width: '100%'}, {justifyContent: 'center'}, {alignSelf: 'center'}, {borderRadius: 10}]}>
+        <Text style={[{fontSize: 21}, {paddingHorizontal: 2}, {paddingVertical: 3}, {color: '#fff'}, {textAlign: 'justify'}]}>
+          ESTA TELA SERÁ LANÇADA EM BREVE COM SUAS FUNCIONALIDADES
+        </Text>
+      </View>
+
+      <ComponentAgenda/>
+
+    </SafeAreaView>
+  );
+}
