056633dbb83e843638f36c35833f42e13ee8bde0
Phillipe Macedo <63377161+Phillipe17Macedo@users.noreply.github.com>
2025-01-06 21:24:18 -0300

    ajustando-tela-more

--- app/(tabs)/more.tsx
+++ app/(tabs)/more.tsx
@@ -1,18 +1,18 @@
 /* eslint-disable prettier/prettier */
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { View, Text, ScrollView } from "react-native";
+import { View, Text, ScrollView, SafeAreaView } from "react-native";
 
 import { styles } from "../../style/StylesMore/styles";
 import OptionsMore from "@/components/OptionsScreenMore/OptionsMore";
 
 export default function More() {
   return (
-    <ScrollView style={styles.container}>
+    <View style={styles.container}>
       <StatusBar style="light" />
-      <Stack.Screen options={{ title: "Mais" }} />
+      <Stack.Screen options={{ title: "Mais Opções" }} />
 
       <OptionsMore />
-    </ScrollView>
+    </View>
   );
 }
